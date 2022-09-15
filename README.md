88f4fdc244b0d75bb5d11fe5864eac6c809f5bdd
AK <akshaykumar71297@gmail.com>
2022-09-15 10:56:14 -0300

    clean up

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # swapi-world
+to run the scipt launch this command: `npm start`
+
+once finished a file (assets) will be created with the results
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { readFileSync, writeFileSync } from 'fs';
-import axios, {AxiosResponse} from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import array, { ceil } from 'lodash';
-import { _setInterval } from './util/utils';
 
 let batchNumber = 0;
 let batchSize = 1000;
@@ -9,75 +8,84 @@ let batchSize = 1000;
 const secretData = readFileSync('src/assets/super-secret-data.txt', 'utf-8');
 const secretDataArray = secretData.split('\n');
 
-const ary = (array.chunk(secretDataArray, ceil(batchSize))).slice(0, 3);  // 101 chunks with aprox. 1000 elements in array
+const chunksOfSecretData = (array.chunk(secretDataArray, ceil(batchSize))).slice(0,2);  // 101 chunks with aprox. 1000 elements in array
 
 let requestPromises: Promise<AxiosResponse<any, any>>[] = [];
-console.log(ary.length);
+
+console.log('Total number of batches: ' + chunksOfSecretData.length);
 
 const intervalID = setInterval(() => {
 
 	let min = batchNumber * 1;
 	let max = (batchNumber+1) * 1;
 	batchNumber++
-	
 
-	ary.slice(min, max).map((elements) => {
-		console.log('here');
-		let body = elements.map((el) => el.replace(/(\r\n|\n|\r)/gm, '')); // cleaning 
+	chunksOfSecretData.slice(min, max).map((elements) => {
+		let body = elements.map((el) => el.replace(/(\r\n|\n|\r)/gm, '').trim()); // cleaning 
 		requestPromises.push(
 			axios.post('https://txje3ik1cb.execute-api.us-east-1.amazonaws.com/prod/decrypt', body, {
-				headers: {
-				'Content-Type': 'application/json',
-				'x-api-key': 'Q76n6BBoa46yWuxYL7By02KcKfOQz0kd9lVflIXZ',
-				},
-				maxContentLength: Infinity,
-				maxBodyLength: Infinity})
+					headers: {
+					'Content-Type': 'application/json',
+					'x-api-key': 'Q76n6BBoa46yWuxYL7By02KcKfOQz0kd9lVflIXZ'
+					}
+				})
 			);
 	})
-	console.log(min, max);
+	console.info('Processing batch: ' + batchNumber);
 
-	if(max >= ary.length) {
+	if(max >= chunksOfSecretData.length) {
+		// stopping the interval
 		clearInterval(intervalID);
-		Promise.all(requestPromises).then((responses) => {
+
+		Promise.allSettled(requestPromises).then((responses) => {
 			if (responses.length > 0) {
-			  writeFileSync('src/example.txt', '', { flag: 'w' });
-			  let gxsk: Record<string, any> = [];
-			  let gxsv: Record<string, any> = [];
-			  responses.forEach((response) => {
-				let resp: Array<any> = response.data; // response.data array of stringed objs
-				let actors = resp.map((actor) => {
-				  return JSON.parse(actor);
+				// keys for each galaxy
+				let gxsk: Record<string, any> = [];
+
+				// values (list of e) for each galaxy
+				let gxsv: Record<string, any> = [];
+				responses.forEach((response) => {
+					if(response.status == 'fulfilled') {
+						let resp = response.value.data; // response.data array of stringed objs
+						let actors = resp.map((actor: any) => {
+							return JSON.parse(actor);
+						});
+						let homeworlds = actors.reduce(function (cb: any, initial: any) {
+							cb[initial.homeworld] = cb[initial.homeworld] || [];
+							cb[initial.homeworld].push(initial.name);
+							return cb;
+						}, {});
+						
+						for (let homeworld in homeworlds) {
+							gxsk.push(homeworld);
+							gxsv.push(homeworlds[homeworld]);
+						}
+					}
 				});
-				let homeworlds = actors.reduce(function (cb, initial) {
-				  cb[initial.homeworld] = cb[initial.homeworld] || [];
-				  cb[initial.homeworld].push(initial.name);
-				  return cb;
-				}, {});
-				
-				for (let homeworld in homeworlds) {
-					gxsk.push(homeworld);
-					gxsv.push(homeworlds[homeworld]);
-				}
-			  });
 		
-			  let final: Record<string, any> = {};
-			  if (gxsk.length > 0) {
-				for (let i in gxsk) {
-				  if (gxsk[i] in final) {
-					final[gxsk[i]] = Array.from(new Set(final[gxsk[i]].concat(gxsv[i])));
-				  } else {
-					final[gxsk[i]] = gxsv[i];
-				  }
+				let final: Record<string, any> = {};
+				if (gxsk.length > 0) {
+					for (let i in gxsk) {
+						if (gxsk[i] in final) {
+							final[gxsk[i]] = Array.from(new Set(final[gxsk[i]].concat(gxsv[i])));
+						} else {
+							final[gxsk[i]] = gxsv[i];
+						}
+					}
 				}
-			  }
-		
-			  console.log(final)
-			  writeFileSync('src/example.txt', JSON.stringify(final, null, "\t"), { flag: 'w' });
+			
+				// for json format
+				// writeFileSync('src/assets/citizens-super-secret-info.txt', JSON.stringify(final, null, "\t"), { flag: 'w' });
+
+				// for cleaner print
+				let cleanPrint = '';
+				for (let galaxyName in final) {
+					let actors = final[galaxyName].map((actor: any) => ` - ${actor}`);
+					cleanPrint += galaxyName + '\n' + actors.join('\n') + '\n\n';
+				}
+				writeFileSync('src/assets/citizens-super-secret-info.txt', cleanPrint, { flag: 'w' });
 			}
-		  })
-		  .catch((err) => console.log('err', err));
+		}).catch((err) => console.log('err', err));
 	}
-}, 1000)
-
-
-  
+}, 500)
+// processing requestss every 500ms to avoid error: To Many requests from Cloludfront
